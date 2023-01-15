9ae0faf6f38c0614425befd3a8bcf1e4ca719604
0utsi <mateuszludyk@gmail.com>
2023-01-15 22:37:34 +0100

    ...

--- src/components/ProductsTable.tsx
+++ src/components/ProductsTable.tsx
@@ -8,42 +8,40 @@ const ProductsTable: FC<ProductProps> = (props) => {
 	const [openedModal, setOpenedModal] = useState<any>(null);
 
 	const openInfoModal = (i) => {
-		setOpenedModal(i);
 		console.log(props.currentPageItems[openedModal]);
+		setOpenedModal(i);
 	};
 
 	return (
-		<table>
-			<thead>
-				<tr className="rowHeader">
-					<th scope="col">ID</th>
-					<th scope="col">name</th>
-					<th scope="col">year</th>
-				</tr>
-			</thead>
-			<tbody>
-				{props.currentPageItems.map((item, i) => {
-					return (
-						<tr
-							onClick={() => openInfoModal(i)}
-							style={{
-								backgroundColor: item.color,
-							}}
-							key={item.id}
-						>
-							<td>{item.id}</td>
-							<td>{item.name}</td>
-							<td>{item.year}</td>
-						</tr>
-					);
-				})}
-			</tbody>
-			{/* {openedModal && (
-				<div className="infoModal">
-					console.log({props.currentPageItems[openedModal]})
-				</div>
-			)} */}
-		</table>
+		<>
+			<table>
+				<thead>
+					<tr className="rowHeader">
+						<th scope="col">ID</th>
+						<th scope="col">name</th>
+						<th scope="col">year</th>
+					</tr>
+				</thead>
+				<tbody>
+					{props.currentPageItems.map((item, i) => {
+						return (
+							<tr
+								onClick={() => openInfoModal(i)}
+								style={{
+									backgroundColor: item.color,
+								}}
+								key={item.id}
+							>
+								<td>{item.id}</td>
+								<td>{item.name}</td>
+								<td>{item.year}</td>
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
+			{openedModal && <div className="infoModal"></div>}
+		</>
 	);
 };
 
